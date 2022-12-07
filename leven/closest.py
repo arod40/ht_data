ceb4d1015b520d4924f2e4c845a6efd6d0493122
arod40 <arodacademic@gmail.com>
2022-12-07 10:42:35 -0600

    Update leven script to new data

--- leven/closest.py
+++ leven/closest.py
@@ -15,9 +15,8 @@ def func(p1, data_index):
     return [levenshtein(p1, p2) / max(len(p1), len(p2)) for p2 in data_index]
 
 
-def find_closests_parallel(csv_path, dump_to_dir, batch_size, slice=(0, None)):
+def find_closests_parallel(data_index, dump_to_dir, batch_size, slice=(0, None)):
     lower, upper = slice
-    data_index = read_csv(csv_path).dropna().post.to_numpy()
     slice_data_index = data_index[lower:upper]
     batches = ceil(len(slice_data_index) / batch_size)
 
@@ -35,7 +34,9 @@ def find_closests_parallel(csv_path, dump_to_dir, batch_size, slice=(0, None)):
 if __name__ == "__main__":
     csv_path = Path(argv[1])
     dump_to_dir = Path(argv[2])
-    slice = int(argv[3]), int(argv[4]) if len(argv) == 5 else None
+    dropna_variant = argv[3]
+    dump_index = argv[4] # set to dump_index to store a csv with the indices
+    slice = int(argv[5]), int(argv[6]) if len(argv) == 7 else None
 
     assert csv_path.exists()
     assert dump_to_dir.exists()
@@ -43,4 +44,18 @@ if __name__ == "__main__":
     slice_dir = dump_to_dir / f"{slice[0]}-{slice[1]}"
     slice_dir.mkdir(exist_ok=True)
 
-    find_closests_parallel(csv_path, slice_dir, cpu_count, slice)
+    df = read_csv(csv_path)
+    if dropna_variant == "108k":
+        df = df.dropna()
+    elif dropna_variant == "350k":
+        df = df[df['title'].notna()]
+        df = df[df['post'].notna()]
+
+    if dump_index == "dump_index":
+        df['index'].to_csv(dump_to_dir/"index.csv")
+
+    data_index = df.post.to_numpy()
+
+    print("Working with data of shape: ", data_index.shape)
+
+    find_closests_parallel(data_index, slice_dir, cpu_count, slice)
